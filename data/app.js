f0acdb28a601d3f268684812f9c7cad044e2226c
Ald121 <fallsouls@hotmail.com>
2016-05-12 17:48:24 -0500

    123
    123

--- data/app.js
+++ data/app.js
@@ -1,26 +1,35 @@
     // create the module and name it scotchApp
-    var app = angular.module('dcApp', ['ui.mask','ngRoute', 'ngAnimate', 'route-segment', 'view-segment', 'ngMaterial']);
+    var app = angular.module('dcApp', ['ui.mask', 'ngRoute', 'ngAnimate', 'route-segment', 'view-segment', 'ngMaterial', 'ngWig']);
 
-    app.factory('service', function($http){
+    var API_URL = 'http://192.168.1.31/api_ex/public/api/v1/';
+
+    app.factory('service', function($http) {
         var service = {
             async: function() {
-                    var promise = $http({
-                        url:"app.php", 
-                        method: 'POST',
-                        data: {methods: 'info'}
-                    }).then(function (response) {
+                var promise = $http({
+                    url: "app.php",
+                    method: 'POST',
+                    data: {
+                        methods: 'info'
+                    }
+                }).then(function(response) {
                     return response.data;
                 });
                 return promise;
             },
-            general:function(typeservices){
-                    var promise = $http.post("app.php", {methods: typeservices}).then(function (response) {
+            general: function(typeservices) {
+                var promise = $http.post("app.php", {
+                    methods: typeservices
+                }).then(function(response) {
                     return response.data;
                 });
                 return promise;
             },
-            general:function(typeservices, url, data){
-                    var promise = $http.post(url, {methods: typeservices, data}).then(function (response) {
+            general: function(typeservices, url, data) {
+                var promise = $http.post(url, {
+                    methods: typeservices,
+                    data
+                }).then(function(response) {
                     return response.data;
                 });
                 return promise;
@@ -29,5 +38,25 @@
         return service;
     });
 
-    
+    app.factory('service', function($http) {
+        var service = {};
+        service.shownoticicas = function() {
+            return $http({
+                url: API_URL + "noticias",
+                method: 'GET'
+            })
+        }
 
+        service.guardar = function($http,datos) {
+            return $http({
+                method: 'POST',
+                url: API_URL + "noticias",
+                data: datos,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+        }
+
+        return service;
+    });
--- data/noticias/controller/app.js
+++ data/noticias/controller/app.js
@@ -1,11 +1,33 @@
 var app = angular.module('dcApp').controller('NoticiasAddCtrl', function($scope){
-   console.log('hola noticias add');
+   console.log('hola noticias add'); 
+
+   // $('[data-toggle="tooltip"]').tooltip();
 });
 var app = angular.module('dcApp').controller('NoticiasPreviewCtrl', function($scope){
    console.log('hola noticias Preview');
 });
-var app = angular.module('dcApp').controller('NoticiasUpdateCtrl', function($scope){
+var app = angular.module('dcApp').controller('NoticiasUpdateCtrl', function($scope,service){
    console.log('hola noticias update');
+var t = $('#tabla-noticias').DataTable();	
+service.shownoticicas().then(function(d) {
+  for (var i = 0; i < d.data.length; i++) {
+  	t.row.add( [
+			d.data[i].id,
+            d.data[i].titulo,
+            d.data[i].URL,
+            d.data[i].referencia,
+			"<a href='#' class='btn-primary bmd-ripple btn-xs' data-toggle='modal' data-target='#modal-update' style='text-decoration: none'><span class='flaticon-create3' style='color:white'> Editar</span></a>\
+                            <a href='#' class='btn-danger bmd-ripple btn-xs' data-toggle='modal' data-target='#modal-delete' style='text-decoration: none'><span class='flaticon-rubbish' style='color:white;'> Eliminar</span></a>"
+		] ).draw( false );
+         }  
+	});
+
+      // service.shownoticicas().then(function(d) {
+
+      //     });
+
+
+
 });
 var app = angular.module('dcApp').controller('NoticiasEditCtrl', function($scope){
    console.log('hola noticias Edit');
