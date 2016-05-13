7f657937ae4bc025e2a95c66046e592796d94bf4
Deivid <deividscriollo@gmail.com>
2016-05-13 17:37:00 -0500

    es

--- data/noticias/controller/app.js
+++ data/noticias/controller/app.js
@@ -3,6 +3,7 @@ var app = angular.module('dcApp').controller('NoticiasAddCtrl', function($scope)
 });
 var app = angular.module('dcApp').controller('NoticiasPreviewCtrl', function($scope){
    console.log('hola noticias Preview');
+   $('[data-toggle="tooltip"]').tooltip();
 });
 var app = angular.module('dcApp').controller('NoticiasUpdateCtrl', function($scope){
    console.log('hola noticias update');
