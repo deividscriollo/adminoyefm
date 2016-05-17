171ccd6a44422d41ac3d8149ebb31456967fb9a8
Ald121 <fallsouls@hotmail.com>
2016-05-17 14:49:08 -0500

    locutores
    locutores

--- data/programas/controller/app.js
+++ data/programas/controller/app.js
@@ -1,10 +1,31 @@
-var app = angular.module('dcApp').controller('ProgramasAddCtrl', function($scope,Programas){
+var app = angular.module('dcApp');
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+
+app.controller('ProgramasAddCtrl', function($scope,Programas){
 console.log('add programa');
+$scope.locutoresarray = [];
 $('#hfin').bootstrapMaterialDatePicker({ time: true, date: false, format: 'hh:mm A', stateColor: 'info' });
 $('#hinicio').bootstrapMaterialDatePicker({ time: true, date: false, format: 'hh:mm A', stateColor: 'info' });
 
 $scope.semana = [{name:'Lunes'},{name:'Martes'},{name:'Miercoles'},{name:'Jueves'},{name:'Viernes'},{name:'Sábado'},{name:'Domingo'}];
     $scope.folder = {};
+
+
 $scope.guardarprograma=function(){
 	    $scope.albumNameArray = [];
 	    angular.forEach($scope.folder,function(key,value){
@@ -12,9 +33,16 @@ $scope.guardarprograma=function(){
                 $scope.albumNameArray.push(value)
         });
         $scope.data["dias"]=$scope.albumNameArray;
+        $scope.data["locutores"]=$scope.locutoresarray;
         $scope.data["hinicio"]=$('#hinicio').val();
         $scope.data["hfin"]=$('#hfin').val();
-       Programas.save($scope.data);
-//console.log($scope.data);
+//        $scope.data["file"]= $scope.myFile;
+        Programas.save($scope.data);
+console.log($scope.data);
+}
+$scope.addlocutor=function(){
+$scope.locutoresarray.push($scope.locutor);
+$scope.locutor="";
+console.log($scope.locutoresarray);
 }
 });
