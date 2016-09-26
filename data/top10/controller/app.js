a02f22d91f5cd2736c133c7fcd0d99a08c06c53e
Ald121 <fallsouls@hotmail.com>
2016-09-26 15:04:00 -0500

    insertar top 10 imagen

--- data/top10/controller/app.js
+++ data/top10/controller/app.js
@@ -2,55 +2,49 @@ var app = angular.module('dcApp');
 
 app.controller('top10AddCtrl', ['$scope', '$location','Upload', '$timeout','servicioTop10', function ($scope,$location, Upload, $timeout,servicioTop10) {
 
+
+$scope.imgarray=[];
 console.log('add Top10');
 $scope.listatop10=[];
 $scope.estadosave=true;
 
-$scope.addcancion=function(){
-  if ($scope.listatop10.length<10) {
+$scope.addcancion=function(file,form){
+  if ($scope.listatop10.length<15) {
   $scope.listatop10.push({
       artista:$scope.data.artista,
       cancion:$scope.data.cancion,
-      url:$scope.data.url
+      url:$scope.data.url,
+      img:file
   });
+  // $scope.listatop10.push($scope.imgarray);
   }
-  if ($scope.listatop10.length==10) {
+  if ($scope.listatop10.length==15) {
     $scope.estadosave=false;
   }
+   if (form) {
+      form.$setPristine();
+      form.$setUntouched();
+      form.$rollbackViewValue();
+    }
+    $scope.data = angular.copy($scope.master);
+    $scope.picFile=angular.copy($scope.master);
 }
 $scope.deletecancion=function(obj){
   var index=$scope.listatop10.indexOf(obj);
       $scope.listatop10.splice(index,1); 
 }
 
-$scope.guardartop10=function(form){
-  servicioTop10.set({top:$scope.listatop10}).$promise.then(function(d){
-    if (d.respuesta) {
-     $scope.listatop10=[];
-    }
-  });
- if (form) {
-      form.$setPristine();
-      form.$setUntouched();
-      form.$rollbackViewValue();
-    }
-    $scope.data = angular.copy($scope.master);
+$scope.delimg=function(index){
+$scope.imgarray.splice(index, 1);
+//console.log($scope.imgarray); 
+}
+
+$scope.guardartop10=function(){
+  Upload.upload({
+      url: 'http://localhost/api-admin-oyefm/public/addTop10',
+      data: {listatop10: $scope.listatop10},
+    });
+    $scope.imgarray=[];
 }
 
 }]);
-
-// app.controller('ProgramasUpdateCtrl', function($scope,Programas){
-//    console.log('hola programa update');
-//     $scope.programas=[];
-//     $scope.getprogramas=function(){
-//     Programas.query().$promise.then(function(data){
-//     $scope.programas=data;
-//      //console.log($scope.programas[1]);
-// }, function(err){
-
-// });
-// }
-// $scope.getprogramas();
-
-
-// });
